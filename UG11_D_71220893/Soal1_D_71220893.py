a558a49031d33d5c0318dfd6b579d00fecafcd76
BayuuAditya <117901006+BayuuAditya@users.noreply.github.com>
2022-12-01 16:16:38 +0700

    Add files via upload

---
+++ UG11_D_71220893/Soal1_D_71220893.py
@@ -0,0 +1,51 @@
+print("Halo Selamat datang di bioskop")
+print("Dimanakah kamu ingin menonton?")
+print("1) XXI Empire")
+print("2) XXI Amplaz")
+print("3) XXI JCM")
+int(input("Masukan Pilihanmu :"))
+def pilihanbioskop():
+    1 
+    print("Mau Nonton Film apa nih? Ada film :")
+
+pilihanbioskop()
+print ("1. Frozen")
+print("2. Keramat")
+print("3. KKN di desa penari")
+int(input("Pilih Nomor Film :"))
+def pilihanfilm():
+    2
+    print("Mau nonton layar bioskop apa:")
+pilihanfilm()
+print("1.Reguler")
+print("2.Dolby almos")
+print("3.Premiere")
+int(input("Pilih nomor tipe layar :"))
+def pilihanlayar():
+    3
+    int(input("Berapa Banyak Tiket? :"))
+pilihanlayar()
+def pilihantiket():
+    2
+    print("Mau nonton jam berapa :")
+pilihantiket()
+print("1) 12.35 WIB")
+print("2) 14.45 WIB")
+print("3) 16.55 WIB")
+print("4) 19.05 WIB")
+x = "12.35 WIB"
+y = "14.45 WIB"
+z = "16.55 WIB"
+w  = "19.05 WIB"
+def pilihanjam():
+    int(input("Masukan angka pilihan anda : ")) 
+    print("oke berhasil, silahkan dinikmati di jam :",x)
+    
+pilihanjam()
+
+
+
+
+
+
+
